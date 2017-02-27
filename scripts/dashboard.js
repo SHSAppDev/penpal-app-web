c7603a7dd95a1d3b22a2b6134d8487ed68d0568c
Ryan Anderson <shnoutyanderson@gmail.com>
2017-02-26 16:15:46 -0800

    Add conversation-list loading. Again. Now with the new UI.

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -8,6 +8,7 @@ function W3Dashboard() {
     // this.userName = document.getElementById('user-name');
     this.signOutButton = document.getElementById('sign-out');
     this.signOutButtonMobile = document.getElementById('sign-out-mobile');
+    this.conversationList = document.getElementById('conversation-list');
 
 
     this.signOutButton.addEventListener('click', this.signOut.bind(this));
@@ -35,9 +36,30 @@ W3Dashboard.prototype.signOut = function() {
     this.auth.signOut();
 };
 
+W3Dashboard.URL_PROFILE_PLACEHOLDER = '/images/profile_placeholder.png';
+
+// Checks if the given userID is registered in user-data, and if it's not,
+// the userID gets registered
+W3Dashboard.prototype.checkForFirstTimeUser = function(userId) {
+  var userRef = this.database.ref('user-data/'+userId);
+  userRef.once('value', function(snapshot) {
+    if(snapshot.val() === null) {
+      // We have ourselves a first time user!
+      // console.log("this="+this);
+      userRef.set({
+        displayName: this.auth.currentUser.displayName,
+        photoURL: this.auth.currentUser.photoURL || W3Dashboard.URL_PROFILE_PLACEHOLDER
+      });
+    }
+  }.bind(this));
+}
+
 // Triggers when the auth state change for instance when the user signs-in or signs-out.
 W3Dashboard.prototype.onAuthStateChanged = function(user) {
     if (user) { // User is signed in!
+        //First, check for first time user.
+        this.checkForFirstTimeUser(this.auth.currentUser.uid);
+
         // Get profile pic and user's name from the Firebase user object.
         var profilePicUrl = user.photoURL; // Get profile pic.
         // var userName = user.displayName;        // Get user's name.
@@ -51,6 +73,8 @@ W3Dashboard.prototype.onAuthStateChanged = function(user) {
         this.userPic.removeAttribute('hidden');
         this.signOutButton.removeAttribute('hidden');
 
+        this.loadConversations();
+
 
     } else { // User is signed out!
 
@@ -59,13 +83,78 @@ W3Dashboard.prototype.onAuthStateChanged = function(user) {
     }
 };
 
+W3Dashboard.prototype.loadConversations = function() {
+  this.myConversationsRef = this.database.ref('user-data/'+this.auth.currentUser.uid+'/conversations');
+  this.myConversationsRef.off();
+  var setConversation = function(data) {
+    // Each item is a random key with a recipientUserID
+    // console.log(this.displayConversation);
+    this.displayConversation(data.val().recipientUID);
+  }
+  this.myConversationsRef.on('child_added', setConversation.bind(this));
+  this.myConversationsRef.on('child_changed', setConversation.bind(this));
+
+};
+
+W3Dashboard.prototype.displayConversation = function(recipientUID) {
+
+  // this.userDataRef.child(recipientUID).once('value', function(snapshot){
+  this.database.ref('user-data/'+recipientUID).once('value').then(function(snapshot){
+    if(snapshot.val()===null) {return;}
+
+    var div = document.getElementById(recipientUID);
+    if(!div) {
+      var container = document.createElement('div');
+      container.innerHTML = W3Dashboard.CONVERSATION_TEMPLATE;
+      div = container.firstChild;
+      div.setAttribute('id', recipientUID);
+      // console.log(this.conversationList); //HELP
+      this.conversationList.appendChild(div);
+    }
+    // Set recipient profile pic in conversation button
+    // var urlForPic = null;
+    // if(snapshot.val().photoURL !== null) {
+    //   urlForPic = snapshot.val().photoURL;
+    // } else {
+    //   urlForPic = W3Dashboard.URL_PROFILE_PLACEHOLDER;
+    // }
+    // // div.querySelector('.pic').style.backgroundImage = 'url(' + urlForPic.replace('"', '') + ')';
+    // div.querySelector('.pic').src = urlForPic;
+    // div.querySelector('#conversation-link').href = 'conversation.html?targetUID='+recipientUID;
+    div.href = 'conversation.html?targetUID='+recipientUID;
+    div.textContent = snapshot.val().displayName;
+    console.log(div.querySelector('#bleh'));
+    // div.querySelector('#bleh').textContent = 1; //Hardcoded value
+
+    // console.log(div.querySelector('.pic').style.backgroundImage);
+
+    // div.querySelector('.name').textContent = snapshot.val().displayName;
+  }.bind(this));
+};
+
+
 // Template for messages.
-W3Dashboard.MESSAGE_TEMPLATE =
-    '<div class="message-container">' +
-    '<div class="spacing"><div class="pic"></div></div>' +
-    '<div class="message"></div>' +
-    '<div class="name"></div>' +
-    '</div>';
+// W3Dashboard.MESSAGE_TEMPLATE =
+//     '<div class="message-container">' +
+//     '<div class="spacing"><div class="pic"></div></div>' +
+//     '<div class="message"></div>' +
+//     '<div class="name"></div>' +
+//     '</div>';
+
+W3Dashboard.CONVERSATION_TEMPLATE =
+    '<a href=# class="collection-item">' +
+      '<span class="new badge" id="bleh"></span>Name'+
+    '</a>';
+
+    // '<div>' +
+    //   // '<div class="spacing"><div class="pic"></div></div>' +
+    //   '<a href=# id="conversation-link">'+
+    //     '<img src=# class="pic">' +
+    //     '<div class="name"></div>' +
+    //   '</a>'
+    // '</div>';
+
+
 
 // A loading image URL.
 W3Dashboard.LOADING_IMAGE_URL = 'https://www.google.com/images/spin-32.gif';
@@ -87,7 +176,7 @@ function getParameterByName(name, url) {
 
 window.onload = function() {
     window.dashboardScript = new W3Dashboard();
-    document.getElementById('.button-collapse').sideNav();
+    // document.getElementById('.button-collapse').sideNav();
 };
 
 (function($) {
