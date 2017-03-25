c527b84a20bf89d085984250eb1a98fe346023df
Kha Nguyen <kha.vng@gmail.com>
2017-03-25 01:19:04 -0700

    Added code to retrieve the user's timezone and time based on their browser. Now, we must add this data to the database.

--- scripts/user-info.js
+++ scripts/user-info.js
@@ -1,6 +1,6 @@
 
 // loads and saves the user's timezone
-function saveTimeZone {
+function getTimeZone {
 
     if (!sessionStorage.getItem('timezone')) {
         var tz = jstz.determine() || 'UTC';
@@ -9,3 +9,44 @@ function saveTimeZone {
     var currTz = sessionStorage.getItem('timezone');
 }
 
+function loadTime() {
+	var now = new Date;
+	var hours;
+	var minutes;
+	if (now.getUTCHours() <= 9) {
+		hours = "0" + now.getUTCHours(); 
+	}
+	
+	else {
+		hours = now.getUTCHours();
+	}
+	
+	if (now.getUTCMinutes() <= 9) {
+		minutes = "0" + now.getUTCMinutes();
+	}
+	
+	else {
+		minutes = now.getUTCMinutes();
+	}
+	
+	console.log("Hours: " + hours);
+	console.log("Minutes: " + minutes);
+	
+	var time = 	hours + ":" + minutes;
+	console.log(time);
+	convertTime(time);
+}
+
+function convertTime(theTime) {
+	var date = moment().format("YYYY-MM-DD");
+	var stamp = date + "T" + theTime + "Z";
+	var momentTime = moment(stamp);
+
+	var tzTime = momentTime.tz(currTz);
+	var formattedTime = tzTime.format('h:mm A');
+
+	console.log("formatted time: " + formattedTime);
+	localTime.textContent = "Time in " + currTz + ": " + formattedTime;
+}
+loadTime();
+setInterval(loadTime, 60*1000);
