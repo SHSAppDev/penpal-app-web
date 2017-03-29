45e51b79b0b562766d5f868b49329c23c2f0fe34
Kha Nguyen <kha.vng@gmail.com>
2017-03-29 02:08:56 -0700

    Fixed timezone code. Should be working as intended, but can't practically test ... for now

--- scripts/user-info.js
+++ scripts/user-info.js
@@ -1,12 +1,13 @@
-
 // loads and saves the user's timezone
-function getTimeZone {
 
-    if (!sessionStorage.getItem('timezone')) {
-        var tz = jstz.determine() || 'UTC';
-        sessionStorage.setItem('timezone', tz.name());
-    }
-    var currTz = sessionStorage.getItem('timezone');
+var currTz;
+
+function getTimeZone() {
+	if (!sessionStorage.getItem('timezone')) {
+		var tz = jstz.determine() || 'UTC';
+		sessionStorage.setItem('timezone', tz.name());
+	}
+	currTz = sessionStorage.getItem('timezone');
 }
 
 function loadTime() {
@@ -14,39 +15,39 @@ function loadTime() {
 	var hours;
 	var minutes;
 	if (now.getUTCHours() <= 9) {
-		hours = "0" + now.getUTCHours(); 
-	}
-	
-	else {
+		hours = "0" + now.getUTCHours();
+	} else {
 		hours = now.getUTCHours();
 	}
-	
+
 	if (now.getUTCMinutes() <= 9) {
 		minutes = "0" + now.getUTCMinutes();
-	}
-	
-	else {
+	} else {
 		minutes = now.getUTCMinutes();
 	}
-	
+
 	console.log("Hours: " + hours);
 	console.log("Minutes: " + minutes);
-	
-	var time = 	hours + ":" + minutes;
+
+	var time = hours + ":" + minutes;
 	console.log(time);
 	convertTime(time);
 }
 
 function convertTime(theTime) {
+	console.log("Converting time");
 	var date = moment().format("YYYY-MM-DD");
 	var stamp = date + "T" + theTime + "Z";
 	var momentTime = moment(stamp);
 
+	getTimeZone();
+	console.log("currTz: " + currTz);
 	var tzTime = momentTime.tz(currTz);
 	var formattedTime = tzTime.format('h:mm A');
 
 	console.log("formatted time: " + formattedTime);
-	localTime.textContent = "Time in " + currTz + ": " + formattedTime;
+	// localTime.textContent = "Time in " + currTz + ": " + formattedTime;
 }
+console.log("got user info");
 loadTime();
-setInterval(loadTime, 60*1000);
+setInterval(loadTime, 60 * 1000);
