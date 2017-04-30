e89d7f4ee0d79748029948021988d898a3b5797d
Ryan Anderson <shnoutyanderson@gmail.com>
2017-04-30 14:04:58 -0700

    Add more special punctuation. Make tools title white and bold and correct margin.

--- scripts/bind-special-character-tool.js
+++ scripts/bind-special-character-tool.js
@@ -6,8 +6,8 @@ var allCharacterButtons = document.getElementsByClassName("special-character-too
 for(var i=0; i<allCharacterButtons.length; i++) {
   var btn = allCharacterButtons[i];
   btn.addEventListener('click', function(){
+    message.focus();
     var char = this.textContent;
     message.value += char;
-    message.focus();
   });
 }
