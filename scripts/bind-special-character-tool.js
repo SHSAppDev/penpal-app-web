ed231fca1358b60f2c4c2ed0211a8fdb010bc9fc
Ryan Anderson <shnoutyanderson@gmail.com>
2017-04-30 13:30:02 -0700

    Add basic special characters tool.

---
+++ scripts/bind-special-character-tool.js
@@ -0,0 +1,13 @@
+
+'use strict';
+
+var message = document.getElementById('message');
+var allCharacterButtons = document.getElementsByClassName("special-character-tool-button");
+for(var i=0; i<allCharacterButtons.length; i++) {
+  var btn = allCharacterButtons[i];
+  btn.addEventListener('click', function(){
+    var char = this.textContent;
+    message.value += char;
+    message.focus();
+  });
+}
