387b2e5c69d62418b6ca85c158f0b1f42cd5d11d
Ryan Anderson <shnoutyanderson@gmail.com>
2017-07-27 15:51:45 -0700

    Special character sets can be set programatically

--- scripts/bind-special-character-tool.js
+++ scripts/bind-special-character-tool.js
@@ -2,12 +2,35 @@
 'use strict';
 
 var message = document.getElementById('message');
-var allCharacterButtons = document.getElementsByClassName("special-character-tool-button");
-for(var i=0; i<allCharacterButtons.length; i++) {
-  var btn = allCharacterButtons[i];
-  btn.addEventListener('click', function(){
+
+// Initialize all the special character things
+
+const BTN_TEMPLATE = '<a class="waves-effect waves-teal btn-flat special-character-tool-button">'+
+  '<span class="notranslate" style="text-transform:none;">INSERT-CHAR</span></a>';
+const charSets = {
+  'es': ['á', 'é', 'í', 'ó', 'ú', 'ñ', '¿', '¡'],
+  'fr': ['é', 'à', 'è', 'ù', 'â', 'ê', 'î', 'ô', 'û', 'ç', 'ë', 'ï','ü'],
+  'de': ['ä', 'Ä', 'é', 'ö', 'Ö', 'ü', 'Ü', 'ß'],
+  'emoji': ['😀','😎','😯','😕','👍','👊','❤️','😆','😅']
+};
+
+function setCharsetInUI(charsetIdentifier){
+  var chars = charSets[charsetIdentifier];
+  var insertHTML = '';
+  for(var i=0; i < chars.length; i++) {
+    const char = chars[i];
+    var btnHTML = BTN_TEMPLATE.replace('INSERT-CHAR', char);
+    insertHTML += btnHTML;
+  }
+  $('#special-characters-panel .collapsible-body').html(insertHTML);
+  $('.special-character-tool-button').click(function(){
+    var character = this.textContent;
     message.focus();
-    var char = this.textContent;
-    message.value += char;
+    message.value += character;
   });
 }
+
+
+
+
+setCharsetInUI('es');
