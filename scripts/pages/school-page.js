99220784d274ff8334ee342286e96e6d00472ff8
Ryan Anderson <shnoutyanderson@gmail.com>
2017-12-19 19:22:05 -0800

    Added add conversation buttons to school page

--- scripts/pages/school-page.js
+++ scripts/pages/school-page.js
@@ -31,6 +31,7 @@ SchoolPage.prototype.onAuthStateChanged = function(user) {
     // firebase.database().ref('schools').once('value', function(data){
     //   console.log('all schools '+data.val())
     // }.bind(this));
+    this.command = new Command();
     firebase.database().ref('user-data/'+firebase.auth().currentUser.uid)
       .once('value', function(snapshot){
         this.myProfile = snapshot.val();
@@ -70,11 +71,38 @@ SchoolPage.prototype.initializeUserListForGivenSchool = function(schoolCode) {
         listItem.querySelector('.display-name').textContent = each.displayName;
         listItem.querySelector('.photo').src = each.photoURL;
         listItem.querySelector('.email').textContent = each.email;
-        if(this.IAlreadyHaveConversationWith(each.uid)) {
-          listItem.querySelector('a.add-conv-button').setAttribute('disabled', true);
-          listItem.querySelector('a.add-conv-button i').innerHTML = 'check';
 
+        const addConvButton = listItem.querySelector('a.add-conv-button');
+        addConvButton.id = 'add-conv-button-'+each.uid;
+        const addConvButtonIcon = listItem.querySelector('a.add-conv-button i');
+        const loader = listItem.querySelector('div.loader-red-small');
+        if(this.IAlreadyHaveConversationWith(each.uid)) { //TODO reneable when done testing.
+          addConvButton.setAttribute('disabled', true);
+          addConvButtonIcon.innerHTML = 'check';
+        } else {
+          console.log('adding click event for addConvButton');
+          $(addConvButton).click(function(){
+            console.log('clicked! '+addConvButton.id);
+            addConvButton.style.display = 'none';
+            loader.style.display = 'block';
+            this.command.requestFunction('addConversation',{recipientUID: each.uid},
+            {
+              success: function(resp) {
+                loader.style.display = 'none';
+                addConvButton.style.display = 'block';
+                addConvButton.setAttribute('disabled', true);
+                addConvButtonIcon.innerHTML = 'check';
+                Materialize.toast('Conversation successfully added!', 2000);
+              }.bind(this),
+              error: function(err) {
+                loader.style.display = 'none';
+                addConvButton.style.display = 'block';
+                Materialize.toast('There was an error in adding the conversation.', 2000);
+              }.bind(this)
+            });
+          }.bind(this));
         }
+
 
         listItems[i] = listItem;
       }
@@ -120,20 +148,23 @@ SchoolPage.SCHOOL_LIST_TEMPLATE =
   '</ul>';
 SchoolPage.USER_LIST_ITEM =
     '<li class="collection-item avatar">' +
-    '<div style="float: left">' +
-    // '<a class="anchor" href=#>' +
-    '<img src=# class="circle photo"></img>' +
-    '<span class="title display-name">Alvin</span>' +
-    '<p class="email"></p>' +
-    // '</a>' +
-    '</div>' +
-    '<a class="add-conv-button btn-floating btn-medium waves-effect waves-light red" style="float: right"><i class="material-icons">person_add</i></a>' +
+      '<div style="float: left">' +
+      // '<a class="anchor" href=#>' +
+        '<img src=# class="circle photo"></img>' +
+        '<span class="title display-name">Alvin</span>' +
+        '<p class="email"></p>' +
+      // '</a>' +
+      '</div>' +
+      '<a class="add-conv-button btn-floating btn-medium waves-effect waves-light red" style="float: right"><i class="material-icons">person_add</i></a>' +
+      '<div class="loader-red-small" style="float: right; display: none"></div>'
     '</li>';
+
 
 // tells us if our current user already has a conversation with the specified uid
 // this.myProfile must be defined before the function is called.
 SchoolPage.prototype.IAlreadyHaveConversationWith = function(uid) {
-  const conversations = Object.values(this.myProfile.conversations);
+  const obj = this.myProfile.conversations;
+  const conversations = obj?Object.values(obj):[];
   for(var i=0; i<conversations.length; i++) {
     const conv = conversations[i];
     if(conv.recipientUID == uid) return true;
